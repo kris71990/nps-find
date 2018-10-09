334bf0c50cba703f0abb2616ed3b8b082de551d3
Kris Sakarias <ksakarias@gmail.com>
2018-10-09 16:23:15 -0700

    working on image display carousel

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -1,14 +1,36 @@
 <template>
   <div v-if="computedParks" id="search-results" >
     <h3>National Parks in {{ computedState }}</h3>
-    <ul id="park-list">
+    <!-- <Carousel v-bind:slides="slides"/> -->
+    <!-- <ul id="park-list">
       <li v-for="item in computedParks" :key="item.id">
         <div v-if="item.imageUrl">
           <img v-bind:src="randomizedImage(item)"/>
         </div>
         <p>{{ item.fullName }}</p>
       </li>
-    </ul>
+    </ul> -->
+    <div class='carousel-view'>
+      <ul id="park-list">
+        <transition-group
+          class='carousel'
+          tag="div">
+          <li
+            v-for="item in computedParks" 
+            class='slide'
+            :key="item.imageUrl">
+            <div v-if="item.imageUrl">
+              <img v-bind:src="randomizedImage(item.imageUrl)"/>
+              <p>{{ item.fullName }}</p>
+            </div>
+            <!-- <div class='carousel-controls' :key="item.imageUrl">
+              <button class='carousel-controls__button' @click="previous">prev</button>
+              <button class='carousel-controls__button' @click="next">next</button>
+            </div> -->
+          </li>
+        </transition-group>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -17,17 +39,27 @@ import { mapState } from 'vuex';
 
 export default {
   name: 'Dashboard',
-  computed: mapState({
-    computedParks: state => state.parks,
-    computedState: state => state.stateFull,
-  }),
+  computed:
+    mapState({
+      computedParks: state => state.parks,
+      computedState: state => state.stateFull,
+    }),
   methods: {
     randomizedImage:
-      (park) => {
-        const arr = park.imageUrl.split('\n');
+      (imageUrl) => {
+        const arr = imageUrl.split('\n');
+        arr.pop()
         const index = Math.round(Math.random() * (arr.length - 1));
         return arr[index];
-      }
+      }, 
+    next () {
+      const first = this.imageArr.shift()
+      imageArr = images.concat(first)
+    },
+    previous () {
+      const last = imageArr.pop()
+      imageArr = [last].concat(imageArr)
+    }
   }
 }
 </script>
---
+++ frontend/src/components/image-carousel.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class='carousel-view'>
+    <transition-group
+      class='carousel'
+      tag="div">
+      <div
+        v-for="slide in this.slides" 
+        class='slide'
+        :key="slide">
+        <img src="slide"/>
+      </div>
+    </transition-group>
+    <div class='carousel-controls'>
+      <button class='carousel-controls__button' @click="previous">prev</button>
+      <button class='carousel-controls__button' @click="next">next</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  props: {
+    slides: Array,
+  },
+  methods: {
+    next () {
+      const first = this.slides.shift()
+      this.slides = this.slides.concat(first)
+    },
+    previous () {
+      const last = this.slides.pop()
+      this.slides = [last].concat(this.slides)
+    }
+  }
+  // methods: {
+  //   randomizedImage:
+  //     (park) => {
+  //       const index = Math.round(Math.random() * (arr.length - 1));
+  //       return arr[index];
+  //     }
+  // }
+}
+</script>
