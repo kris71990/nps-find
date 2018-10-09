b748065c43ac9d5070000a5e2471ea9dafee840d
Kris Sakarias <ksakarias@gmail.com>
2018-10-08 17:46:38 -0700

    jest config

---
+++ backend/src/__test__/lib/jest-setup.js
@@ -0,0 +1,5 @@
+'use strict';
+
+require('dotenv').config({ 
+  path: `${__dirname}/.env`,
+});
--- backend/src/__test__/lib/test.env.js
+++
@@ -1,3 +0,0 @@
-process.env.NODE_ENV = 'development';
-process.env.PORT = 9000;
-process.env.DATABASE_URL = 'postgres://localhost:5432/npstest';
