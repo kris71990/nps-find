7ee5035a876e3f3bbc7707ff4d836ad19fa84bd5
Kris Sakarias <ksakarias@gmail.com>
2018-10-22 18:48:39 -0700

    refactoring customization

--- backend/src/routes/park.js
+++ backend/src/routes/park.js
@@ -5,63 +5,65 @@ import logger from '../lib/logger';
 import models from '../models';
 
 import getData from '../lib/get-parks';
+import customizeParks from '../lib/customize-parks';
 
 const parkRouter = new Router();
 
 parkRouter.get('/parks/:state', (request, response, next) => {
   logger.log(logger.INFO, `Processing a get for /parks/${request.params.state}...`);
 
-  let reqArr;
-  if (request.query.interests && typeof request.query.interests === 'string') {
-    reqArr = [request.query.interests];
-  } else {
-    reqArr = request.query.interests;
-  }
+  const parks = customizeParks(request.query);
+  // let reqArr;
+  // if (request.query.interests && typeof request.query.interests === 'string') {
+  //   reqArr = [request.query.interests];
+  // } else {
+  //   reqArr = request.query.interests;
+  // }
 
-  const interestParkTypes = [];
+  // const interestParkTypes = [];
 
-  if (reqArr) {
-    for (let i = 0; i < reqArr.length; i++) {
-      if (reqArr[i] === 'history') {
-        interestParkTypes.push('National Military Park');
-        interestParkTypes.push('National Battlefield');
-        interestParkTypes.push('National Battlefield Park');
-        interestParkTypes.push('National Battlefield Site');
-        interestParkTypes.push('National Historic Area');
-        interestParkTypes.push('National Heritage Area');
-        interestParkTypes.push('National Historic Site');
-        interestParkTypes.push('National Historic Park');
-        interestParkTypes.push('National Historical Park and Preserve');
-        interestParkTypes.push('National Historic Trail');
-        interestParkTypes.push('International Historic Site');
-        interestParkTypes.push('National Memorial');
-      } else if (reqArr[i] === 'hiking') {
-        interestParkTypes.push('National Recreation Area');
-        interestParkTypes.push('National Preserve');
-        interestParkTypes.push('National Geologic Trail');
-        interestParkTypes.push('National Park & Preserve');
-        interestParkTypes.push('National Park');
-        interestParkTypes.push('National Historic Trail');
-        interestParkTypes.push('National Reserve');
-        interestParkTypes.push('National Trail System');
-      } else if (reqArr[i] === 'nature') {
-        interestParkTypes.push('National Preserve');
-        interestParkTypes.push('National Geologic Trail');
-        interestParkTypes.push('National Park & Preserve');
-        interestParkTypes.push('National Park');
-        interestParkTypes.push('Wild River');
-        interestParkTypes.push('National Parkway');
-        interestParkTypes.push('National Monument & Preserve');
-        interestParkTypes.push('National Lakeshore');
-        interestParkTypes.push('National Seashore');
-        interestParkTypes.push('National Reserve');
-        interestParkTypes.push('National River');
-        interestParkTypes.push('National Wild & Scenic River & Riverway');
-      }
-    }
-  }
+  // if (reqArr) {
+  //   for (let i = 0; i < reqArr.length; i++) {
+  //     if (reqArr[i] === 'history') {
+  //       interestParkTypes.push('National Military Park');
+  //       interestParkTypes.push('National Battlefield');
+  //       interestParkTypes.push('National Battlefield Park');
+  //       interestParkTypes.push('National Battlefield Site');
+  //       interestParkTypes.push('National Historic Area');
+  //       interestParkTypes.push('National Heritage Area');
+  //       interestParkTypes.push('National Historic Site');
+  //       interestParkTypes.push('National Historic Park');
+  //       interestParkTypes.push('National Historical Park and Preserve');
+  //       interestParkTypes.push('National Historic Trail');
+  //       interestParkTypes.push('International Historic Site');
+  //       interestParkTypes.push('National Memorial');
+  //     } else if (reqArr[i] === 'hiking') {
+  //       interestParkTypes.push('National Recreation Area');
+  //       interestParkTypes.push('National Preserve');
+  //       interestParkTypes.push('National Geologic Trail');
+  //       interestParkTypes.push('National Park & Preserve');
+  //       interestParkTypes.push('National Park');
+  //       interestParkTypes.push('National Historic Trail');
+  //       interestParkTypes.push('National Reserve');
+  //       interestParkTypes.push('National Trail System');
+  //     } else if (reqArr[i] === 'nature') {
+  //       interestParkTypes.push('National Preserve');
+  //       interestParkTypes.push('National Geologic Trail');
+  //       interestParkTypes.push('National Park & Preserve');
+  //       interestParkTypes.push('National Park');
+  //       interestParkTypes.push('Wild River');
+  //       interestParkTypes.push('National Parkway');
+  //       interestParkTypes.push('National Monument & Preserve');
+  //       interestParkTypes.push('National Lakeshore');
+  //       interestParkTypes.push('National Seashore');
+  //       interestParkTypes.push('National Reserve');
+  //       interestParkTypes.push('National River');
+  //       interestParkTypes.push('National Wild & Scenic River & Riverway');
+  //     }
+  //   }
+  // }
 
-  const reducedParkTypes = [...new Set(interestParkTypes)];
+  // const reducedParkTypes = [...new Set(interestParkTypes)];
 
   // find if state exists in db
   return models.state.findAll({
@@ -74,7 +76,7 @@ parkRouter.get('/parks/:state', (request, response, next) => {
       if (results.length > 0) {
         logger.log(logger.INFO, `Returning park data from db for ${request.params.state}`);
 
-        if (reducedParkTypes.length === 0) {
+        if (parks.length === 0) {
           return models.park.findAll({
             where: {
               stateCode: request.params.state,
@@ -87,7 +89,7 @@ parkRouter.get('/parks/:state', (request, response, next) => {
         return models.park.findAll({
           where: {
             stateCode: request.params.state,
-            designation: reducedParkTypes,
+            designation: parks,
           },
         })
           .then((retrievedParks) => {
@@ -98,7 +100,7 @@ parkRouter.get('/parks/:state', (request, response, next) => {
       // if it doesn't, call this function to get data from the api
       return getData(request.params.state)
         .then(() => {
-          if (reducedParkTypes.length === 0) {
+          if (parks.length === 0) {
             return models.park.findAll({
               where: {
                 stateCode: request.params.state,
@@ -111,7 +113,7 @@ parkRouter.get('/parks/:state', (request, response, next) => {
           return models.park.findAll({
             where: {
               stateCode: request.params.state,
-              designation: reducedParkTypes,
+              designation: parks,
             },
           })
             .then((retrievedParks) => {
