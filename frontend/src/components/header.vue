6e306a6d5d4695953abe9beb6252ca8af2a1739a
Kris Sakarias <ksakarias@gmail.com>
2018-10-26 16:09:51 -0700

    added header logo and style

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -1,13 +1,18 @@
 <template>
   <header class="header">
-    <h1>Explore American Public Lands</h1>
-    <nav>
-      <ul>
-        <router-link to="/"><li v-on:click='resetDefault'>Home</li></router-link>
-        <li><a href="#">Profile</a></li>
-        <li><a href="#">About</a></li>
-      </ul>
-    </nav>
+    <div id="header-img">
+      <img src="../utils/bison.png">
+    </div>
+    <div id="header-nav">
+      <h1>| Explore American Public Lands |</h1>
+      <nav>
+        <ul>
+          <router-link to="/"><li v-on:click='resetDefault'>Home</li></router-link>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -26,21 +31,53 @@ export default {
 header {
   background-color: #336E55;
   color: #F1E3CB;
-  border: 2px solid black;
-  a {
-    text-decoration: none;
-    color: black;
-    font-weight: bold;
+  border: 3px solid black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  #header-img {
+    width: 10%;
+    padding: 5px 20px 0px 20px;
+    img {
+      width: 100%;
+      margin-bottom: 0;
+    }
   }
-  a:hover {
-    color: #F1E3CB;
-  }
-  ul {
-    padding-left: 0px;
-    li {
-      display: inline;
-      margin: 2%;
-      list-style-type: none;
+  #header-nav {
+    width: 88%;
+    h1 {
+      width: 45%;
+      height: 100%;
+      float: left;
+      text-align: left;
+      font-style: italic;
+      padding-left: 5%;
+      text-decoration: underline overline dotted #5E9B7E;
+    }
+    nav {
+      width: 40%;
+      height: 100%;
+      float: left;
+      text-align: right;
+      font-style: italic;
+      padding-right: 8%;
+      ul {
+        padding-left: 0px;
+        padding-top: 10%;
+        li {
+          display: inline;
+          margin: 2%;
+          list-style-type: none;
+        }
+        a {
+          text-decoration: none;
+          color: black;
+          font-weight: bold;
+        }
+        a:hover {
+          color: #F1E3CB;
+        }
+      }
     }
   }
 }
