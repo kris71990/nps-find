f451cef359952a3280242382bbaa61af714bc9da
Kris Sakarias <ksakarias@gmail.com>
2018-10-26 18:39:35 -0700

    fix bug with filtering park types

--- backend/src/lib/customize-parks.js
+++ backend/src/lib/customize-parks.js
@@ -11,18 +11,32 @@ const history = [
   'National Heritage Area',
   'National Historic Site',
   'National Historic Park',
+  'National Historical Park',
   'National Historical Park and Preserve',
+  'National Historical Reserve',
   'National Historic Trail',
   'International Historic Site',
   'National Memorial',
+  'Memorial Parkway',
+  'Cultural Heritage Corridor',
+  'National Monument and Historic Shrine',
+  'National Monument',
+  'Ecological & Historic Preserve',
+  'National Heritage Corridor',
+  'Other',
 ];
 
 const hiking = [
   'National Recreation Area',
   'National Preserve',
   'National Geologic Trail',
+  'National Scenic Trail',
   'National Park & Preserve',
+  'National and State Parks',
+  'International Park',
   'National Park',
+  'National Parks',
+  'Park',
   'National Historic Trail',
   'National Reserve',
   'National Trail System',
@@ -32,15 +46,26 @@ const nature = [
   'National Preserve',
   'National Geologic Trail',
   'National Park & Preserve',
+  'National Recreation Area',
+  'International Park',
+  'Park',
   'National Park',
+  'National Parks',
   'Wild River',
   'National Parkway',
   'National Monument & Preserve',
+  'National and State Parks',
   'National Lakeshore',
   'National Seashore',
   'National Reserve',
   'National River',
   'National Wild & Scenic River & Riverway',
+  'National River & Recreation Area',
+  'Parkway',
+  'National Monument',
+  'Affiliated Area',
+  'Ecological & Historic Preserve',
+  'National Scenic Trail',
 ];
 
 const customizeParks = (query) => {
@@ -52,18 +77,30 @@ const customizeParks = (query) => {
   }
 
   logger.log(logger.INFO, `Filtering for user preferences: ${[...reqArr]}`);
-  let interestParkTypes = [];
+  const interestParkTypes = [];
 
   if (reqArr) {
-    for (let i = 0; i < reqArr.length; i++) {
-      if (reqArr[i] === 'history') {
-        interestParkTypes = history;
-      } else if (reqArr[i] === 'hiking') {
-        interestParkTypes = hiking;
-      } else if (reqArr[i] === 'nature') {
-        interestParkTypes = nature;
+    reqArr.forEach((interest) => {
+      if (interest === 'history') {
+        history.forEach((type) => {
+          if (interestParkTypes.indexOf(type) < 0) {
+            interestParkTypes.push(type);
+          }
+        });
+      } else if (interest === 'hiking') {
+        hiking.forEach((type) => {
+          if (interestParkTypes.indexOf(type) < 0) {
+            interestParkTypes.push(type);
+          }
+        });
+      } else if (interest === 'nature') {
+        nature.forEach((type) => {
+          if (interestParkTypes.indexOf(type) < 0) {
+            interestParkTypes.push(type);
+          }
+        });
       }
-    }
+    });
   }
 
   return interestParkTypes;
