568aceab4358ecc0495acd873c47596306386803
Kris Sakarias <ksakarias@gmail.com>
2018-11-02 01:07:37 -0700

    debugging login token parsing in basic auth

--- backend/src/lib/basic-auth-middleware.js
+++ backend/src/lib/basic-auth-middleware.js
@@ -1,31 +1,31 @@
 'use strict';
 
-import bcrypt from 'bcrypt';
 import HttpError from 'http-errors';
 import models from '../models/index';
 
-const verifyPassword = function verifyPassword(password, account) {
-  return bcrypt.compare(password, account.passwordHash)
-    .then((result) => {
-      if (!result) {
-        throw new HttpError(400, 'AUTH - incorrect password');
-      }
-      return this;
-    });
-};
+// const verifyPassword = function verifyPassword(password, account) {
+//   return bcrypt.compare(password, account.passwordHash)
+//     .then((result) => {
+//       if (!result) {
+//         throw new HttpError(400, 'AUTH - incorrect password');
+//       }
+//       return this;
+//     });
+// };
 
 export default (request, response, next) => {
+  console.log(request.headers.authorization);
   if (!request.headers.authorization) {
     return next(new HttpError(400, 'AUTH - invalid request'));
   }
 
   const base64AuthHeader = request.headers.authorization.split('Basic ')[1];
+  console.log(base64AuthHeader);
   if (!base64AuthHeader) {
     return next(new HttpError(400, 'AUTH - invalid request'));
   }
 
   const stringAuthHeader = Buffer.from(base64AuthHeader, 'base64').toString();
-  console.log(stringAuthHeader);
   const [username, password] = stringAuthHeader.split(':');
 
   if (!username || !password) {
@@ -33,15 +33,13 @@ export default (request, response, next) => {
   }
 
   console.log(username);
-  return models.account.findOne({ 
-    where: { username },
-  })
+  return models.account.findOne({ where: { username } })
     .then((account) => {
-      console.log(account);
       if (!account) {
-        return next(new HttpError(400, 'AUTH - invalid request'));
+        return next(new HttpError(400, 'AUTH -invalid request'));
       }
-      return verifyPassword(password, account);
+      console.log(account);
+      return account.verifyPassword(password);
     })
     .then((account) => {
       request.account = account;
--- backend/src/models/account.js
+++ backend/src/models/account.js
@@ -1,54 +1,69 @@
 'use strict';
 
-// import bcrypt from 'bcrypt';
-// import crypto from 'crypto';
-// import jsonWebToken from 'jsonwebtoken';
-// import HttpError from 'http-errors';
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+import jsonWebToken from 'jsonwebtoken';
+import HttpError from 'http-errors';
 
-// const HASH_ROUNDS = 8;
-// const TOKEN_SEED_LENGTH = 128;
+const HASH_ROUNDS = 8;
+const TOKEN_SEED_LENGTH = 128;
 
-const Account = (sequelize, DataTypes) => sequelize.define('account', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  passwordHash: {
-    type: DataTypes.STRING(1000),
-    allowNull: false,
-  },
-  tokenSeed: {
-    type: DataTypes.STRING(1000),
-    allowNull: false,
-    unique: true,
-  },
-});
+export default (sequelize, DataTypes) => {
+  const Account = sequelize.define('account', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    passwordHash: {
+      type: DataTypes.STRING(1000),
+      allowNull: false,
+    },
+    tokenSeed: {
+      type: DataTypes.STRING(1000),
+      allowNull: false,
+      unique: true,
+    },
+  });
 
-// Account.generateToken = function generateToken() {
-//   this.tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
-//   return this.save()
-//     .then((account) => {
-//       return jsonWebToken.sign({
-//         tokenSeed: account.tokenSeed,
-//       }, process.env.ACCOUNT_SECRET);
-//     })
-//     .catch(() => new HttpError(401, 'Error creating token'));
-// };
+  Account.generateToken = function (account) { // eslint-disable-line
+    account.tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
+    return account.save()
+      .then(() => {
+        return jsonWebToken.sign({
+          tokenSeed: account.tokenSeed,
+        }, process.env.ACCOUNT_SECRET);
+      })
+      .catch(() => new HttpError(401, 'Error creating token'));
+  };
+  
+  Account.verifyPassword = function (password, account) { // eslint-disable-line
+    return bcrypt.compare(password, account.passwordHash)
+      .then((result) => {
+        if (!result) {
+          throw new HttpError(400, 'AUTH - incorrect password');
+        }
+        return account;
+      });
+  };
 
-// Account.verifyPassword = function verifyPassword(password) {
-//   return bcrypt.compare(password, this.passwordHash)
-//     .then((result) => {
-//       if (!result) {
-//         throw new HttpError(400, 'AUTH - incorrect password');
-//       }
-//       return this;
-//     });
-// };
-
-export default Account;
+  Account.createAccount = function (username, email, password) { // eslint-disable-line
+    return bcrypt.hash(password, HASH_ROUNDS)
+      .then((passwordHash) => {
+        password = null; //eslint-disable-line
+        const tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
+        return Account.create({
+          username,
+          email,
+          passwordHash,
+          tokenSeed,
+        });
+      });
+  };
+  return Account;
+};
