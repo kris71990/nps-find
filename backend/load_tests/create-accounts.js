b80d1ee51c467cec41f63262a98da7c33c219004
Kris Sakarias <ksakarias@gmail.com>
2018-12-13 13:10:43 -0800

    add simple account load test

---
+++ backend/load_tests/create-accounts.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const faker = require('faker');
+
+const loadTestUser = module.exports = {};
+
+loadTestUser.create = (userContext, events, done) => {
+  userContext.vars.username = faker.internet.userName() + Math.random().toString();
+  userContext.vars.email = faker.internet.email();
+  userContext.vars.password = faker.internet.password() + Math.random.toString();
+  return done();
+};
